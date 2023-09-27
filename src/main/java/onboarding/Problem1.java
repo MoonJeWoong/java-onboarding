dd251d36fb71908b028f001a441b01757348e1b2
Maljaaa <Maljaaa@github.com>
2023-09-27 15:40:07 +0900

    [java-onboarding] 신승민 problem1

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,72 +4,88 @@ import java.util.List;
 
 class Problem1 {
 
+    private static final int EXCEPTION_NUMBER = -1;
+    private static final int DRAW_NUMBER = 0;
+    private static final int WIN_POBI_NUMBER = 1;
+    private static final int WIN_CRONG_NUMBER = 2;
+    private static final int FIRST_PAGE_OF_BOOK = 1;
+    private static final int LAST_PAGE_OF_BOOK = 400;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
 
-        // 예외사항 -1
-        if(pobi.get(0) + 1 != pobi.get(1) || crong.get(0) + 1 != crong.get(1)) {
-            answer = -1;
+        // Exception
+        if(isNotTwoSize(pobi) || isNotBookPage(pobi) || isNotSidePage(pobi)) {
+            return EXCEPTION_NUMBER;
+        } else if (isNotTwoSize(crong) || isNotBookPage(crong) || isNotSidePage(crong)) {
+            return EXCEPTION_NUMBER;
         }
 
-        // pobi 결과
-        int pobiResult = getPobiResult(pobi);
+        int pobiScore = getScore(pobi);
+        int crongScore = getScore(crong);
 
-        // crong 결과
-        int crongResult = getCrongResult(crong);
-
-        // 승부
-        answer = getAnswer(answer, pobiResult, crongResult);
+        Integer drawNumber = getResult(pobiScore, crongScore);
+        if (drawNumber != null) return drawNumber;
 
         return answer;
     }
 
-    private static int getAnswer(int answer, int pobiResult, int crongResult) {
-        if(pobiResult > crongResult) {
-            answer = 1;
-        } else if (pobiResult < crongResult) {
-            answer = 2;
-        } else if (pobiResult == crongResult) {
-            answer = 0;
+    private static Integer getResult(int pobiScore, int crongScore) {
+        if(pobiScore == crongScore) {
+            return DRAW_NUMBER;
+        } else if (pobiScore > crongScore) {
+            return WIN_POBI_NUMBER;
+        } else {
+            return WIN_CRONG_NUMBER;
         }
-        return answer;
     }
 
-    private static int getCrongResult(List<Integer> crong) {
-        int crongResult = 0;
+    private static int getScore(List<Integer> list) {
+        int leftSum = getSum(list.get(0));
+        int leftMul = getMul(list.get(0));
+        int rightSum = getSum(list.get(1));
+        int rightMul = getMul(list.get(1));
 
-        for(int i = 0; i < 2; i ++){
-            if( (int)(Math.log10(crong.get(i) + 1)) == 2) {
-                int mul = (crong.get(i) / 10) * (crong.get(i) % 10);
-                int sum = (crong.get(i) / 10) + (crong.get(i) % 10);
+        int left = Math.max(leftSum, leftMul);
+        int right = Math.max(rightSum, rightMul);
 
-                crongResult = sum > mul ? sum : mul;
-            } else {
-                int sum = (crong.get(i) / 100) + (crong.get(i) / 10) + (crong.get(i) % 10);
-                int mul = (crong.get(i) / 100) * (crong.get(i) / 10) * (crong.get(i) % 10);
-
-                crongResult = sum > mul ? sum : mul;
-            }
-        }
-        return crongResult;
+        return Math.max(left, right);
     }
 
-    private static int getPobiResult(List<Integer> pobi) {
-        int pobiResult = 0;
+    private static int getMul(Integer integer) {
+        int result = 0;
 
-        for(int i = 0; i < 2; i ++){
-            if( (int)(Math.log10(pobi.get(i) + 1)) == 2) {
-                int sum = (pobi.get(i) / 10) + (pobi.get(i) % 10);
-                int mul = (pobi.get(i) / 10) * (pobi.get(i) % 10);
-
-                pobiResult = sum > mul ? sum : mul;
-            } else {
-                int sum = (pobi.get(i) / 100) + (pobi.get(i) / 10) + (pobi.get(i) % 10);
-                int mul = (pobi.get(i) / 100) * (pobi.get(i) / 10) * (pobi.get(i) % 10);
-
-                pobiResult = sum > mul ? sum : mul;
-            }
+        while(integer != 0) {
+            result *= integer % 10;
+            integer /= 10;
         }
-        return pobiResult;
+
+        return result;
     }
+
+    private static int getSum(Integer integer) {
+        int result = 0;
+
+        while(integer != 0) {
+            result += integer % 10;
+            integer /= 10;
+        }
+
+        return result;
+    }
+
+    private static boolean isNotSidePage(List<Integer> list) {
+        return list.get(0) != list.get(1) - 1;
+    }
+
+    private static boolean isNotBookPage(List<Integer> list) {
+        return list.get(0) < FIRST_PAGE_OF_BOOK || list.get(0) > LAST_PAGE_OF_BOOK ||
+                list.get(1) < FIRST_PAGE_OF_BOOK || list.get(1) > LAST_PAGE_OF_BOOK;
+    }
+
+    private static boolean isNotTwoSize(List<Integer> list) {
+        return list.size() != 2;
+    }
+
+
 }
